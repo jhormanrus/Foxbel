f34f3ec4bafdda2a3a1a2074a81bb5a351608a64
Jhorman Tito <jhormanrus@gmail.com>
2023-07-10 00:16:20 +0000

    bump dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,18 +4,12 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 module.exports = {
   root: true,
   extends: [
-    "plugin:vue/vue3-recommended",
+    "plugin:vue/vue3-essential",
     "eslint:recommended",
-    "@vue/eslint-config-typescript/recommended",
-    "@vue/eslint-config-prettier",
+    "@vue/eslint-config-typescript"
   ],
-  env: {
-    "vue/setup-compiler-macros": true,
-  },
-  overrides: [
-    {
-      files: ["cypress/integration/**.spec.{js,ts,jsx,tsx}"],
-      extends: ["plugin:cypress/recommended"],
-    },
-  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    project: ['./tsconfig.app.json', './tsconfig.node.json', './tsconfig.vitest.json']
+  }
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
-/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{vue,ts}"],
   theme: {
     extend: {},
-  },
-  plugins: [require("@tailwindcss/line-clamp")],
+  }
 };
